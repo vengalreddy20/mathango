320c6a2e26073f89b534b0605cd4e9c8d29e7f01
vengalreddy2000 <vengalareddy@theretailinsights.com>
2024-06-19 21:13:48 +0530

    fixes

---
+++ src/constant/recipies.ts
@@ -0,0 +1,27 @@
+export const recipies = [
+  {
+    image: "/images/recipe-bg.jpg",
+    name: "Shahi Paneer",
+    time: "Ready in 25 min",
+  },
+  {
+    image: "/images/recipe-bg.jpg",
+    name: "Shahi Paneer",
+    time: "Ready in 25 min",
+  },
+  {
+    image: "/images/recipe-bg.jpg",
+    name: "Shahi Paneer",
+    time: "Ready in 25 min",
+  },
+  {
+    image: "/images/recipe-bg.jpg",
+    name: "Shahi Paneer",
+    time: "Ready in 25 min",
+  },
+  {
+    image: "/images/recipe-bg.jpg",
+    name: "Shahi Paneer",
+    time: "Ready in 25 min",
+  },
+];
